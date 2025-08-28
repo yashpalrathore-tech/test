94bd9349446b7c6c8f949f3aef0e847366d3878d
Yashpal <yashpalrathore366@gmail.com>
2025-08-28 17:49:46 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-         yashpal =============
+         yashpal
         </p>
       </div>
       <p className="read-the-docs">
